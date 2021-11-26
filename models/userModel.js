70dfc8ed30034b2737621813e98ad554bbb4edc9
afhaam <afhaam@pop-os.localdomain>
2021-11-26 23:13:10 +0530

    cart added final project

--- models/userModel.js
+++ models/userModel.js
@@ -13,7 +13,6 @@ const userSchema = new mongoose.Schema({
         number: { type: String, required: true },
         pincode: { type: String, required: true },
         street: { type: String, required: true },
-        address: { type: String, required: true },
         district: { type: String, required: true },
         state: { type: String, required: true },
         landmark:{type:String,required:true}
